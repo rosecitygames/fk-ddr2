cae8cc22dca63e6895c70aa41b18ccfd355d961b
dpid <dlux@dluxgames.com>
2019-05-06 06:22:16 -0700

    Add card data collection interface

---
+++ Assets/FloppyKnightsDemo/Scripts/Cards/ICardDataCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FloppyKnights.Cards
+{
+    public interface ICardDataCollection
+    {
+        List<ICardData> CardDatas { get; }
+        void AddCard(ICardData cardData);
+        void RemoveCard(ICardData cardData);
+        void Clear();
+        void Shuffle();
+    }
+}
